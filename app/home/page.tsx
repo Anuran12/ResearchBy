61972e7eb3a90eedc455e3d83d6e7c53bd1e0a21
Anuran12 <anuranarts@gmail.com>
2025-01-17 18:27:53 +0530

    contact

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Works from "@/components/Works";
@@ -11,6 +12,7 @@ function HomePage() {
       <Hero />
       <About />
       <Works />
+      <Contact />
     </div>
   );
 }
