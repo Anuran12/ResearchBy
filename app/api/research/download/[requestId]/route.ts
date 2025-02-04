255ccf49871d3cd95b6f4868685026aee32d6e41
Anuran12 <anuranarts@gmail.com>
2025-02-04 14:02:15 +0530

    fix

--- app/api/research/download/[requestId]/route.ts
+++ app/api/research/download/[requestId]/route.ts
@@ -1,25 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-type Props = {
-  params: {
-    requestId: string;
-  };
-};
-
-export async function GET(req: NextRequest, { params }: Props) {
+export async function GET(
+  request: NextRequest,
+  context: { params: { requestId: string } }
+) {
   try {
     const response = await fetch(
-      `http://ec2-54-177-139-194.us-west-1.compute.amazonaws.com:3000/api/research/download/${params.requestId}`
+      `http://ec2-54-177-139-194.us-west-1.compute.amazonaws.com:3000/api/research/download/${context.params.requestId}`
     );
     const contentType = response.headers.get("content-type");
     const blob = await response.blob();
 
-    return new NextResponse(blob, {
+    return new Response(blob, {
       headers: {
         "Content-Type": contentType || "application/octet-stream",
       },
     });
   } catch {
-    return NextResponse.json({ error: "Failed to download" }, { status: 500 });
+    return Response.json({ error: "Failed to download" }, { status: 500 });
   }
 }
